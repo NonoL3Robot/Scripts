ae848ffdf69d64c1f0179bda4448328f094df1ee
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2023-10-18 19:49:45 +0200

    edit @supportURL

--- Youtube/AntiAdblockPopupRemover.user.js
+++ Youtube/AntiAdblockPopupRemover.user.js
@@ -7,7 +7,7 @@
 // @author          NonoL3Robot (https://github.com.com/NonoL3Robot/)
 // @updateURL       https://raw.githubusercontent.com/NonoL3Robot/Youtube/AntiAdblockPopupRemover.user.js
 // @downloadURL     https://raw.githubusercontent.com/NonoL3Robot/Youtube/AntiAdblockPopupRemover.user.js
-// @supportURL      https://github.com.com/NonoL3Robot/Scripts/issues
+// @supportURL      https://github.com/NonoL3Robot/Scripts/issues
 // @match           https://youtube.com/watch?v=*
 // @grant           none
 // @compatible      chrome
